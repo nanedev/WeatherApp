102be1051dfa6f49866299c612d44f22d5ddc231
Alen <tutarisalen@gmail.com>
2021-02-24 22:35:54 +0100

    add value converterter

---
+++ WeatherApp/WeatherApp/Converters/BackgroundColorConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace WeatherApp.Converters
+{
+    public class BackgroundColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string color = "";
+
+            int temp = int.Parse((string)value);
+
+            if (temp >= 0 && temp <=10)
+            {
+                color = "#0000FF";
+            }
+            else if (temp > 10 && temp <= 15)
+            {
+                color = "#FFFF00";
+            }
+            else if (temp > 15 && temp <= 25)
+            {
+                color = "#FFA500";
+            }
+            else if (temp > 25)
+            {
+                color = "#FF0000";
+            }
+
+            return color;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
